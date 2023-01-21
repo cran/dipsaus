0591178c4161ba8e0b900020c7d23f6d28a3f66b
Zhengjia Wang <dipterix.wang@gmail.com>
2023-01-21 11:30:02 +0000

    version 0.2.6

--- R/reexports.R
+++ R/reexports.R
@@ -5,3 +5,31 @@ digest::digest
 #' @importFrom parallel detectCores
 #' @export
 parallel::detectCores
+
+#' @importFrom cli ansi_strip
+#' @export
+cli::ansi_strip
+
+#' @importFrom cli tree
+#' @export
+cli::tree
+
+#' @importFrom rlang entrace
+#' @export
+rlang::entrace
+
+#' @importFrom rlang cnd_entrace
+#' @export
+rlang::cnd_entrace
+
+#' @importFrom rlang abort
+#' @export
+rlang::abort
+
+#' @importFrom rlang error_cnd
+#' @export
+rlang::error_cnd
+
+#' @importFrom rlang trace_back
+#' @export
+rlang::trace_back
